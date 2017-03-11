bb3050bb6c27c4ed76aea71c4bad2e42a0d0ad27
Alvaro <alvaro@chowned.iptv.microsoft.com>
2017-03-11 04:57:27 +0000

    deleted _id from models

--- models/player.js
+++ models/player.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 var playerSchema = Schema({
-	_id: Schema.Types.ObjectId,
 	_externalId: String,
 	name: String,
 	position: String,
--- models/team.js
+++ models/team.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 var teamSchema = Schema({
-	_id: Schema.Types.ObjectId,
 	_externalId: String,
 	name: String,
 	active: Boolean,
